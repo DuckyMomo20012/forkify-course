b4cae236ae3da3d19ef874fc260801e7da49cb1a
duckymomo20012 <tienvinh.duong4@gmail.com>
2021-12-20 16:44:48 +0700

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '7da6610a-1533-44be-bb71-9aea2350e5ae';
+export const MODAL_CLOSE_SEC = 2.5;
